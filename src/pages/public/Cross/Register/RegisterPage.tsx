8ad62afd47ee9265d8467c5ee8c8525b0062f1d6
Marlon Daniel <marlondaniel.art@gmail.com>
2025-07-06 23:10:35 -0300

    feat: add components to Register Page

--- src/pages/public/Cross/Register/RegisterPage.tsx
+++ src/pages/public/Cross/Register/RegisterPage.tsx
@@ -1,5 +1,30 @@
+import { AuthCard } from '@/components/auth/AuthCard';
+import { Button } from '@/components/Button';
+import { MyField } from '@/components/MyField';
+import { Link } from 'react-router';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <AuthCard title="Cadastre-se na Syki" helperText="Enviaremos um e-mail de confirmação">
+      <MyField.Root>
+        <MyField.Label>E-mail</MyField.Label>
+        <MyField.Input sizes="large" autoFocus placeholder="Digite seu e-mail" />
+        <MyField.ErrorText>ErrorText do input</MyField.ErrorText>
+      </MyField.Root>
+      <Button classNames="w-full" size="large">
+        Registrar
+      </Button>
+      <p className="text-center text-sm font-medium text-normal">
+        Já tem uma conta?{' '}
+        <Link
+          to="/login"
+          className="text-action-primary hover:underline focus-visible:underline focus-visible:outline-none"
+        >
+          Faça login
+        </Link>
+      </p>
+    </AuthCard>
+  );
 };
 
 export default RegisterPage;
