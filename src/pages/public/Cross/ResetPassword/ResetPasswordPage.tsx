7ca0fde9ddcddd190323a0a99e79ff3502d6cf20
Marlon Daniel <marlondaniel.art@gmail.com>
2025-07-06 23:16:35 -0300

    feat: add components to Reset Password Page

--- src/pages/public/Cross/ResetPassword/ResetPasswordPage.tsx
+++ src/pages/public/Cross/ResetPassword/ResetPasswordPage.tsx
@@ -1,5 +1,99 @@
+import { AuthCard } from '@/components/auth/AuthCard';
+import { CharacterVerification } from '@/components/auth/CharacterVerification';
+import { Button } from '@/components/Button';
+import { IconButton } from '@/components/IconButton';
+import { MyField } from '@/components/MyField';
+import { IconEye, IconEyeClosed } from '@tabler/icons-react';
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type isValidPasswordProps = {
+  numbers: boolean;
+  lowercase: boolean;
+  uppercase: boolean;
+  'eight-characters': boolean;
+  'special-characters': boolean;
+  isAllValid: boolean;
+};
+
 const ResetPasswordPage = () => {
-  return <div>ResetPasswordPage</div>;
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [isValidPassword, setIsValidPassword] = useState<isValidPasswordProps>({
+    numbers: false,
+    lowercase: false,
+    uppercase: false,
+    'eight-characters': false,
+    'special-characters': false,
+    isAllValid: false,
+  });
+
+  return (
+    <AuthCard title="Redefina sua senha">
+      <MyField.Root className="relative">
+        <MyField.Label>Nova senha</MyField.Label>
+        <MyField.Input
+          sizes="large"
+          placeholder="Digite a nova senha"
+          type={showPassword ? 'text' : 'password'}
+          className="pr-10"
+        />
+        <IconButton
+          icon={showPassword ? IconEyeClosed : IconEye}
+          variant={showPassword ? 'light' : 'ghost'}
+          color={showPassword ? 'primary' : 'neutral'}
+          classNames="absolute top-[1.875rem] right-1"
+          size="small"
+          onClick={() => setShowPassword(prev => !prev)}
+        />
+        <MyField.ErrorText>ErrorText do input</MyField.ErrorText>
+      </MyField.Root>
+      <MyField.Root className="relative">
+        <MyField.Label>Confirmar senha</MyField.Label>
+        <MyField.Input
+          sizes="large"
+          placeholder="Confirme a nova senha"
+          type="password"
+          className="pr-10"
+        />
+        <MyField.ErrorText>ErrorText do input</MyField.ErrorText>
+      </MyField.Root>
+      <div
+        className={twMerge(
+          'space-y-2 rounded-lg px-3 py-2.5',
+          isValidPassword.isAllValid ? 'bg-green-50' : 'bg-bg_secondary',
+        )}
+      >
+        <CharacterVerification
+          isValid={isValidPassword.numbers}
+          mainText="Números"
+          leftText="0 - 9"
+        />
+        <CharacterVerification
+          isValid={isValidPassword.lowercase}
+          mainText="Letras minúsculas"
+          leftText="a - z"
+        />
+        <CharacterVerification
+          isValid={isValidPassword.uppercase}
+          mainText="Letras maiúsculas"
+          leftText="A - Z"
+        />
+        <CharacterVerification
+          isValid={isValidPassword['eight-characters']}
+          mainText="8 ou mais caracteres"
+          leftText="••••••••"
+        />
+        <CharacterVerification
+          isValid={isValidPassword['special-characters']}
+          mainText="Caracteres especiais"
+          leftText="!@#$%^&*"
+        />
+      </div>
+      <Button classNames="w-full" size="large" disabled={!isValidPassword.isAllValid}>
+        Redefinir
+      </Button>
+    </AuthCard>
+  );
 };
 
 export default ResetPasswordPage;
