70219b68628774930603ca70d8173d194c6eeeb0
Marlon Daniel <marlondaniel.art@gmail.com>
2025-07-06 23:08:05 -0300

    feat: add components do Login Mfa Page

--- src/pages/public/Cross/LoginMfa/LoginMfaPage.tsx
+++ src/pages/public/Cross/LoginMfa/LoginMfaPage.tsx
@@ -1,5 +1,12 @@
+import { AuthCard } from '@/components/auth/AuthCard';
+import { MyPinInput } from '@/components/MyPinInput';
+
 const LoginMfaPage = () => {
-  return <div>LoginMfaPage</div>;
+  return (
+    <AuthCard title="Código do Google Authenticator" helperText="Informe os 6 dígitos para entrar">
+      <MyPinInput />
+    </AuthCard>
+  );
 };
 
 export default LoginMfaPage;
