d273964ef921cdc7d2ef232a43a7389c516aeb1f
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-07-15 20:27:36 -0300

    feat: add react-hook-form and zod

--- src/pages/public/Cross/Register/RegisterPage.tsx
+++ src/pages/public/Cross/Register/RegisterPage.tsx
@@ -1,47 +1,75 @@
-import { useState } from 'react';
+import { z } from 'zod';
 import { Link } from 'react-router';
 import { Show } from '@/components/Show';
 import { Alert } from '@/components/Alert';
 import { Button } from '@/components/Button';
 import { MyField } from '@/components/MyField';
 import { AuthCard } from '@/components/auth/AuthCard';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { IconAlertTriangle } from '@tabler/icons-react';
 import { usePendingRegisterMutation } from '@/features/Cross/PendingRegister/PendingRegisterClient';
 
+const registerSetupSchema = z.object({
+  email: z.email({ error: 'Informe seu e-mail' }),
+});
+
+type RegisterSetupSchema = z.infer<typeof registerSetupSchema>;
+
 const RegisterPage = () => {
-  const [email, setEmail] = useState('');
+  const { control, handleSubmit } = useForm<RegisterSetupSchema>({
+    resolver: zodResolver(registerSetupSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
   const { mutate, isPending, isError, error } = usePendingRegisterMutation();
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    mutate({ email });
-  };
+  function onSubmit(formValues: RegisterSetupSchema) {
+    mutate({ email: formValues.email });
+  }
 
   return (
-    <AuthCard title="Cadastre-se na Syki" helperText="Enviaremos um e-mail de confirmação">
-      <MyField.Root>
-        <MyField.Label>E-mail</MyField.Label>
-        <MyField.Input
-          sizes="large"
-          autoFocus
-          placeholder="Digite seu e-mail"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <MyField.ErrorText>ErrorText do input</MyField.ErrorText>
-      </MyField.Root>
+    <AuthCard title="Cadastre-se no Syki" helperText="Enviaremos um e-mail de confirmação">
+
+      <Controller
+        control={control}
+        name="email"
+        render={({ field, fieldState }) => (
+          <MyField.Root>
+            <MyField.Label>E-mail</MyField.Label>
+            <MyField.Input
+              sizes="large"
+              autoFocus
+              placeholder="Digite seu e-mail"
+              value={field.value}
+              onChange={field.onChange}
+            />
+            <MyField.ErrorText>{fieldState.error?.message}</MyField.ErrorText>
+          </MyField.Root>
+        )}
+      />
+
       <Show when={isError}>
         <Alert
           icon={IconAlertTriangle}
-          title={error?.message ?? 'Email inválido'}
+          title={error?.message ?? 'E-mail inválido'}
           variant="light"
           color="error"
           showCloseButton={false}
         />
       </Show>
-      <Button classNames="w-full" size="large" onClick={handleSubmit}>
-        {isPending ? 'Registrando...' : 'Registrar'}
+
+      <Button
+        classNames="w-full"
+        size="large"
+        onClick={handleSubmit(onSubmit)}
+        disabled={isPending}
+      >
+        Registrar
       </Button>
+
       <p className="text-center text-sm font-medium text-t-muted">
         Já tem uma conta?{' '}
         <Link
